1c791b7a2df0805ce8fc3beba384b018fd1af0f3
otsoniemi <otso.niemi04@gmail.com>
2023-01-12 13:57:16 +0200

    simplified print statements.

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import split_gif
 import combine_frames
 
 def main():
-    print("Welcome to the Animation Tool")
+    print("FRAMEMASTER")
     print("1. Split gif")
     print("2. Combine frames")
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        gif_path = input("Enter the path of the gif file: ")
-        output_folder = input("Enter the path of the output folder: ")
+        gif_path = input("Enter the file PATH: ")
+        output_folder = input("Enter the output PATH: ")
         split_gif.split_gif(gif_path, output_folder)
     elif choice == "2":
-        input_folder = input("Enter the path of the input folder: ")
-        output_folder = input("Enter the path of the output folder: ")
-        output_format = input("Enter the desired output format (gif or mp4): ")
-        fps_amount = float(input("Enter the desired amount of FPS: "))
+        input_folder = input("Enter the input PATH: ")
+        output_folder = input("Enter the output PATH: ")
+        output_format = input("Enter the format (GIF / MP4): ")
+        fps_amount = float(input("Enter the amount of FPS: "))
         combine_frames.combine_frames(input_folder, output_folder, output_format, fps_amount)
     else:
         print("Invalid choice")
